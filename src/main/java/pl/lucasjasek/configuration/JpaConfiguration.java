b6ce4665f0055aa0ca631a6ad78b7eb5107e2b3c
ljasek <lukasz.jasek@soprasteria.com>
2019-10-26 22:34:46 +0200

    upgrade spring version

--- src/main/java/pl/lucasjasek/configuration/JpaConfiguration.java
+++ src/main/java/pl/lucasjasek/configuration/JpaConfiguration.java
@@ -4,7 +4,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
